e406cf13dec035e092852ad4daabd7bb714ba5d0
emillypalombo-blip <emilly.palombo@escola.pr.gov.br>
2026-04-27 08:22:48 -0300

    v 10
    v 10

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-let frases=["A fé é a certeza das coisas que se esperam",
-"O choro pode durar uma noite, mas a alegria vem pela manhã.",
-"Aqueles que esperam no Senhor renovam as suas forças."];
+let frases = ["Tudo é possível ao que crê ✨",
+    "O amor é paciente, o amor é bondoso. 💕",
+    "Seja feita a tua vontade, assim na terra como no céu. 🙏"];
 
-function geradorMensagem(){
-let indiceGerado= Math.floor(Math.random()*(frases.length));
-let fraseGerada= frases[indiceGerado];
-document.querySelector("#saída").textContent=fraseGerada;
-
-
-
-
+let frasesCopiada = [...frase];
+function geradorMensagem() {
+    if (frases.length == 0) {
+        frases = [...frasesCopiada];
+    }
+    let indiceGerado = Math.floor(Math.random() * (frases.length));
+    let fraseGerada = frases[indiceGerado];
+    console.log(fraseGerada)
+    document.querySelector("#msg").textContent = fraseGerada;
 }
+geradorMensagem()
